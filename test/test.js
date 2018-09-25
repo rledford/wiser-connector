156a8744ca09b2dba7315d2f0b421d47a23171c4
rledford <ryan.c.ledford@gmail.com>
2018-09-24 22:39:20 -0500

    added custom zones and changed update interval

--- test/test.js
+++ test/test.js
@@ -4,11 +4,45 @@ const wc = require('../lib/wiser-connector');
 
 const connector = wc.createConnector();
 
+// two adjacent zones that are 100x100 inches
+const zones = [
+  {
+    name: 123,
+    shape: []
+  },
+  {
+    name: 'Custom Zone A',
+    shape: [
+      { x: 0, y: 0 },
+      { x: 100, y: 0 },
+      { x: 100, y: 100 },
+      { x: 0, y: 100 }
+    ]
+  },
+  {
+    name: 'Custom Zone B',
+    shape: [
+      { x: 100, y: 0 },
+      { x: 200, y: 0 },
+      { x: 200, y: 100 },
+      { x: 100, y: 100 }
+    ]
+  }
+];
+
+connector.on('error', err => {
+  console.log(err);
+});
 connector.on('tagUpdate', tag => {
-  console.log('tag update', tag);
+  //console.log('tag update', tag);
 });
 connector.on('tagZoneTransition', transition => {
   console.log('tag zone transition', transition);
 });
 
-connector.start({ reportTagZoneTransitions: false });
+connector.start({
+  reportTagZoneTransitions: true,
+  reportTagUpdates: false,
+  updateInterval: 2000,
+  zones
+});
