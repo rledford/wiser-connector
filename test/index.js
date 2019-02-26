9884c237f45ce8f38717fd93951156805eaf9db9
rledford <ryan.c.ledford@gmail.com>
2019-02-26 09:31:30 -0600

    updated test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const { WiserConnector, eventType } = require('../dist');
+const { WiserConnector } = require('../dist');
 
 const connector = new WiserConnector();
 
-const { HOSTNAME, PORT } = process.env;
-const TLS_ENABLED = process.env.TLS_ENABLED === 'true' ? true : false;
+const {
+  HOSTNAME,
+  PORT,
+  TAG_HEARTBEAT,
+  TAG_SAMPLE_RATE,
+  TLS_ENABLED
+} = process.env;
 
 connector.on('error', err => {
   console.log(err);
@@ -17,4 +22,10 @@ connector.on('tagHeartbeat', transition => {
   console.log('tag heartbeat', transition);
 });
 
-connector.start({ tagHeartbeat: 10000, tagSampleRate: 1000 });
+connector.start({
+  hostname: HOSTNAME || '127.0.0.1',
+  tlsEnabled: TLS_ENABLED === 'true',
+  port: PORT ? parseInt(PORT) : 3101,
+  tagHeartbeat: TAG_HEARTBEAT || 1000,
+  tagSampleRate: TAG_SAMPLE_RATE || 1000
+});
